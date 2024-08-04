867cbb67133fe945e085314213b32d3233a7d18f
Gabriel Klein <gabriel.klein@philips.com>
2024-08-04 15:02:55 -0300

    Criado os services para o orderservice

---
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
@@ -0,0 +1,19 @@
+package br.com.microservices.orchestrated.orderservice.core.service;
+
+import br.com.microservices.orchestrated.orderservice.core.document.Event;
+import br.com.microservices.orchestrated.orderservice.core.repository.EventRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class EventService {
+
+    private final EventRepository repository;
+
+    public Event save(Event event) {
+        return repository.save(event);
+    }
+}
