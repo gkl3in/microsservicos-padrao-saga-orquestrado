182b63201780fde2d141cad2453f32892c4de1c9
Gabriel Klein <gabriel.klein@philips.com>
2024-08-04 15:41:41 -0300

    Criado os endpoints para realizar um pedido e buscar events

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/consumer/EventConsumer.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/consumer/EventConsumer.java
@@ -1,5 +1,6 @@
 package br.com.microservices.orchestrated.orderservice.core.consumer;
 
+import br.com.microservices.orchestrated.orderservice.core.service.EventService;
 import br.com.microservices.orchestrated.orderservice.core.utils.JsonUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,15 +12,16 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class EventConsumer {
 
+    private final EventService service;
     private final JsonUtil jsonUtil;
 
     @KafkaListener(
-        groupId = "${spring.kafka.consumer.group-id}",
-        topics = "${spring.kafka.topic.notify-ending}"
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.notify-ending}"
     )
     public void consumeNotifyEndingEvent(String payload) {
         log.info("Receiving ending notification event {} from notify-ending topic", payload);
         var event = jsonUtil.toEvent(payload);
-        log.info(event.toString());
+        service.notifyEnding(event);
     }
 }
